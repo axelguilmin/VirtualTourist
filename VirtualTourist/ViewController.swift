d0f56bffe7a9598c01577ee9a0a162eaa2d2d3f9
Axel Guilmin <github@axelguilmin.fr>
2015-09-10 13:14:08 +0200

    First commit - Create the storyboard

---
+++ VirtualTourist/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  VirtualTourist
+//
+//  Created by Axel Guilmin on 9/10/15.
+//  Copyright (c) 2015 Axel Guilmin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
